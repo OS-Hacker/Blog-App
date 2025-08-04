93efd6fe9befe606a818d6c7e82707ed9e428a05
OS-Hacker <os363612@gmail.com>
2025-08-04 12:46:52 +0530

    Final commit

--- server/index.js
+++ server/index.js
@@ -51,11 +51,11 @@ app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-// // Deployment setup
-// // Production configuration
-// if (process.env.isDeployMode === "PRODUCTION") {
-//   app.use(express.static(path.join(__dirname, "../client/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-//   });
-// }
+// Deployment setup
+// Production configuration
+if (process.env.isDeployMode === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
